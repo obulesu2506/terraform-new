d884af59e4e2bf9fe37323d5fa1a9cea1ac7cb53
Obulesu <obulesu.1406@gmail.com>
2025-02-01 16:39:37 +0530

    updated

--- loops/count/ec2.tf
+++ loops/count/ec2.tf
@@ -18,14 +18,14 @@ resource "aws_security_group" "allow_tls" {
   ingress {
     from_port   = var.from_port
     to_port     = var.to_port
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = var.cidr_blocks
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = var.cidr_blocks
   }
 
