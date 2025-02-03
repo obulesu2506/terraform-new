c02859609b4cd35bfa6f1fe5f3331348b608554b
Obulesu <obulesu.1406@gmail.com>
2025-02-03 18:05:36 +0530

    locals example

---
+++ locals/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  description = "instance type version"
+}
+
+variable "project" {
+    default = "expense"
+  
+}
+
+variable "component" {
+    default = "backend"
+  
+}
+
+variable "environment" {
+  default = "dev"
+}
